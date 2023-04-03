13f4659cce5ab64f27850f518dcef6704c188137
Kevin <10223408@student.uts.edu.au>
2023-04-04 02:00:20 +0800

    debug

--- app.py
+++ app.py
@@ -14,6 +14,21 @@ SERVICE_ACCOUNT_KEY = os.environ['SERVICE_ACCOUNT_KEY'].replace('\\n', '\n')
 CALENDAR_ID = os.environ['CALENDAR_ID']
 TIMEZONE = os.environ['TIMEZONE']
 
+print('CALENDAR_ID:', os.environ['CALENDAR_ID'])
+print('TIMEZONE:', os.environ['TIMEZONE'])
+
+print('SERVICE_ACCOUNT_EMAIL:', os.environ['SERVICE_ACCOUNT_EMAIL'])
+print('SERVICE_ACCOUNT_KEY:', os.environ['SERVICE_ACCOUNT_KEY'])
+print('CALENDAR_ID:', os.environ['CALENDAR_ID'])
+print('TIMEZONE:', os.environ['TIMEZONE'])
+print('PROJECT_ID:', os.environ['PROJECT_ID'])
+print('PRIVATE_KEY_ID:', os.environ['PRIVATE_KEY_ID'])
+print('CLIENT_ID:', os.environ['CLIENT_ID'])
+print('AUTH_URI:', os.environ['AUTH_URI'])
+print('TOKEN_URI:', os.environ['TOKEN_URI'])
+print('AUTH_PROVIDER_X509_CERT_URL:', os.environ['AUTH_PROVIDER_X509_CERT_URL'])
+print('CLIENT_X509_CERT_URL:', os.environ['CLIENT_X509_CERT_URL'])
+
 # 設定 Service Account Credentials
 creds = Credentials.from_service_account_info({
     "type": "service_account",
@@ -37,6 +52,8 @@ def list_todays_events():
     # 設定當天的時間範圍
     today_start = datetime.combine(datetime.today(), time.min).isoformat()
     today_end = datetime.combine(datetime.today(), time.max).isoformat()
+    print('today_start:', today_start)
+    print('today_end:', today_end)
 
     # 使用 Calendar API 取得當天的預約
     try:
