641def398f9586c316094fb63e32c53ca7d7715f
Kevin <10223408@student.uts.edu.au>
2023-04-04 18:29:02 +0800

    debug

--- app.py
+++ app.py
@@ -10,24 +10,24 @@ app = Flask(__name__)
 # 從環境變數中讀取 Google 相關變數
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 SERVICE_ACCOUNT_EMAIL = os.environ['SERVICE_ACCOUNT_EMAIL']
-SERVICE_ACCOUNT_KEY = os.environ['SERVICE_ACCOUNT_KEY'].replace('\\n', '\n')
-CALENDAR_ID = os.environ['CALENDAR_ID']
-TIMEZONE = os.environ['TIMEZONE']
+##SERVICE_ACCOUNT_KEY = os.environ['SERVICE_ACCOUNT_KEY'].replace('\\n', '\n')
+##CALENDAR_ID = os.environ['CALENDAR_ID']
+##TIMEZONE = os.environ['TIMEZONE']
 
-print('CALENDAR_ID:', os.environ['CALENDAR_ID'])
-print('TIMEZONE:', os.environ['TIMEZONE'])
-
-print('SERVICE_ACCOUNT_EMAIL:', os.environ['SERVICE_ACCOUNT_EMAIL'])
-print('SERVICE_ACCOUNT_KEY:', os.environ['SERVICE_ACCOUNT_KEY'])
-print('CALENDAR_ID:', os.environ['CALENDAR_ID'])
-print('TIMEZONE:', os.environ['TIMEZONE'])
-print('PROJECT_ID:', os.environ['PROJECT_ID'])
-print('PRIVATE_KEY_ID:', os.environ['PRIVATE_KEY_ID'])
-print('CLIENT_ID:', os.environ['CLIENT_ID'])
-print('AUTH_URI:', os.environ['AUTH_URI'])
-print('TOKEN_URI:', os.environ['TOKEN_URI'])
-print('AUTH_PROVIDER_X509_CERT_URL:', os.environ['AUTH_PROVIDER_X509_CERT_URL'])
-print('CLIENT_X509_CERT_URL:', os.environ['CLIENT_X509_CERT_URL'])
+##print('CALENDAR_ID:', os.environ['CALENDAR_ID'])
+##print('TIMEZONE:', os.environ['TIMEZONE'])
+##
+##print('SERVICE_ACCOUNT_EMAIL:', os.environ['SERVICE_ACCOUNT_EMAIL'])
+##print('SERVICE_ACCOUNT_KEY:', os.environ['SERVICE_ACCOUNT_KEY'])
+##print('CALENDAR_ID:', os.environ['CALENDAR_ID'])
+##print('TIMEZONE:', os.environ['TIMEZONE'])
+##print('PROJECT_ID:', os.environ['PROJECT_ID'])
+##print('PRIVATE_KEY_ID:', os.environ['PRIVATE_KEY_ID'])
+##print('CLIENT_ID:', os.environ['CLIENT_ID'])
+##print('AUTH_URI:', os.environ['AUTH_URI'])
+##print('TOKEN_URI:', os.environ['TOKEN_URI'])
+##print('AUTH_PROVIDER_X509_CERT_URL:', os.environ['AUTH_PROVIDER_X509_CERT_URL'])
+##print('CLIENT_X509_CERT_URL:', os.environ['CLIENT_X509_CERT_URL'])
 
 # 設定 Service Account Credentials
 ##creds = Credentials.from_service_account_info({
