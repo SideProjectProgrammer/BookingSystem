08a53fe8ee0753e407afa7acab239b8630a3b5c9
Kevin <10223408@student.uts.edu.au>
2023-04-10 00:17:56 +0800

    debug

--- app.py
+++ app.py
@@ -11,24 +11,21 @@ app = Flask(__name__)
 
 # 從環境變數中讀取 Google 相關變數
 SCOPES = ['https://www.googleapis.com/auth/calendar']
-SERVICE_ACCOUNT_EMAIL = os.environ['SERVICE_ACCOUNT_EMAIL']
-SERVICE_ACCOUNT_KEY = os.environ['SERVICE_ACCOUNT_KEY'].replace('\\n', '\n')
 CALENDAR_ID = os.environ['CALENDAR_ID']
 TIMEZONE = os.environ['TIMEZONE']
 
 # 設定 Service Account Credentials
-creds = Credentials.from_service_account_info({
-    "type": "service_account",
-    "project_id": os.environ['PROJECT_ID'],
-    "private_key_id": os.environ['PRIVATE_KEY_ID'],
-    "private_key": SERVICE_ACCOUNT_KEY,
-    "client_email": SERVICE_ACCOUNT_EMAIL,
-    "client_id": os.environ['CLIENT_ID'],
-    "auth_uri": os.environ['AUTH_URI'],
-    "token_uri": os.environ['TOKEN_URI'],
-    "auth_provider_x509_cert_url": os.environ['AUTH_PROVIDER_X509_CERT_URL'],
-    "client_x509_cert_url": os.environ['CLIENT_X509_CERT_URL']
-}, scopes=SCOPES, subject=SERVICE_ACCOUNT_EMAIL)
+creds = None
+creds_filename = 'credentials.json'
+
+if os.path.exists(creds_filename):
+    with open(creds_filename, 'r') as f:
+        creds_info = json.load(f)
+        creds = Credentials.from_service_account_info(creds_info, scopes=SCOPES)
+
+if creds is None:
+    print('No credentials found. Exiting...')
+    exit()
 
 # 設定 Calendar API client
 calendar_service = build('calendar', 'v3', credentials=creds)
