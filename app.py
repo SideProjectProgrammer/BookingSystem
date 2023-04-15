e945151cfd60c8aa0758ac4e1658ece4236a879d
Kevin <10223408@student.uts.edu.au>
2023-04-16 00:21:20 +0800

    list today's free time

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from googleapiclient.errors import HttpError
 from googleapiclient.discovery import build
 from flask import Flask, jsonify
 import json
+import sys
 
 app = Flask(__name__)
 
@@ -14,6 +15,8 @@ app = Flask(__name__)
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 CALENDAR_ID = os.environ['CALENDAR_ID']
 TIMEZONE = os.environ['TIMEZONE']
+##CALENDAR_ID = 'chang.yu.chao@gmail.com'
+##TIMEZONE = 'Asia/Taipei'
 
 # 設定 Service Account Credentials
 creds = None
@@ -35,16 +38,16 @@ calendar_service = build('calendar', 'v3', credentials=creds)
 @app.route('/')
 def list_todays_events():
     # 設定目標時區
-    TARGET_TIMEZONE = tz.gettz(os.environ['TIMEZONE'])
+    TARGET_TIMEZONE = tz.gettz(TIMEZONE)
 
     # 取得當前時間
     now = datetime.datetime.utcnow().replace(tzinfo=tz.UTC).astimezone(TARGET_TIMEZONE)
 
-    # 設定今天早上 8 點的 UTC 時間
-    start_of_day = now.replace(hour=8, minute=0, second=0, microsecond=0, tzinfo=pytz.utc)
+    # 設定今天早上 8 點的 TARGET_TIMEZONE 時間
+    start_of_day = now.replace(hour=8, minute=0, second=0, microsecond=0)
 
-    # 設定今天晚上 10 點的 UTC 時間
-    end_of_day = now.replace(hour=22, minute=0, second=0, microsecond=0, tzinfo=pytz.utc)
+    # 設定今天晚上 10 點的 TARGET_TIMEZONE 時間
+    end_of_day = now.replace(hour=22, minute=0, second=0, microsecond=0)
 
     # 取得今天的事件
     events_result = calendar_service.events().list(
@@ -57,9 +60,6 @@ def list_todays_events():
 
     events = events_result.get('items', [])
     
-    if not events:
-        # 如果沒有任何事件發生，直接回傳五個預設時間區間
-        return jsonify({'free_time': ['08:00 - 09:59', '10:00 - 11:59', '14:00 - 15:59', '16:00 - 17:59', '19:00 - 20:590']})
 
     # 計算忙碌時間
     busy_times = []
@@ -76,36 +76,46 @@ def list_todays_events():
         {'time_slot': '19:00 - 20:59', 'free': True}
     ]
 
+    cal_time = []
+    
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
         end = event['end'].get('dateTime', event['end'].get('date'))
         start_time = datetime.datetime.fromisoformat(start).astimezone(TARGET_TIMEZONE).time()
         end_time = datetime.datetime.fromisoformat(end).astimezone(TARGET_TIMEZONE).time()
         for free_time in free_time_list:
-            if start_time < datetime.datetime.strptime(free_time['time_slot'].split(' - ')[1], '%H:%M').time().replace(tzinfo=TARGET_TIMEZONE) and end_time < datetime.datetime.strptime(free_time['time_slot'].split(' - ')[0], '%H:%M').time().replace(tzinfo=TARGET_TIMEZONE):
-                free_time['free'] = True
-                print('start_time:', start_time)
-                print('time_slot start:', datetime.datetime.strptime(free_time['time_slot'].split(' - ')[1], '%H:%M').time().replace(tzinfo=TARGET_TIMEZONE))
-                print('end_time:', end_time)
-                print('time_slot end:', datetime.datetime.strptime(free_time['time_slot'].split(' - ')[0], '%H:%M').time().replace(tzinfo=TARGET_TIMEZONE))
-            elif start_time > datetime.datetime.strptime(free_time['time_slot'].split(' - ')[1], '%H:%M').time().replace(tzinfo=TARGET_TIMEZONE) and end_time > datetime.datetime.strptime(free_time['time_slot'].split(' - ')[0], '%H:%M').time().replace(tzinfo=TARGET_TIMEZONE):
-                free_time['free'] = True
-                print('2start_time:', start_time)
-                print('2time_slot start:', datetime.datetime.strptime(free_time['time_slot'].split(' - ')[1], '%H:%M').time().replace(tzinfo=TARGET_TIMEZONE))
-                print('2end_time:', end_time)
-                print('2time_slot end:', datetime.datetime.strptime(free_time['time_slot'].split(' - ')[0], '%H:%M').time().replace(tzinfo=TARGET_TIMEZONE))
-            else:
+            cal_time.append(datetime.datetime.fromisoformat(start).astimezone(TARGET_TIMEZONE).time().strftime(("%H:%M:%S")))
+            cal_time.append(datetime.datetime.strptime(free_time['time_slot'].split(' - ')[0], '%H:%M').time().replace(tzinfo=TARGET_TIMEZONE).strftime(("%H:%M:%S")))
+            cal_time.append(datetime.datetime.fromisoformat(end).astimezone(TARGET_TIMEZONE).time().strftime(("%H:%M:%S")))
+            cal_time.append(datetime.datetime.strptime(free_time['time_slot'].split(' - ')[1], '%H:%M').time().replace(tzinfo=TARGET_TIMEZONE).strftime(("%H:%M:%S")))
+            if not (start_time < datetime.datetime.strptime(free_time['time_slot'].split(' - ')[0], '%H:%M').time().replace(tzinfo=TARGET_TIMEZONE) and end_time < datetime.datetime.strptime(free_time['time_slot'].split(' - ')[1], '%H:%M').time().replace(tzinfo=TARGET_TIMEZONE)) and not (start_time > datetime.datetime.strptime(free_time['time_slot'].split(' - ')[0], '%H:%M').time().replace(tzinfo=TARGET_TIMEZONE) and end_time > datetime.datetime.strptime(free_time['time_slot'].split(' - ')[1], '%H:%M').time().replace(tzinfo=TARGET_TIMEZONE)):
                 free_time['free'] = False
-
+            cal_time.append(free_time['free'])
+            cal_time.append("===")
 
     free_time_list = [free_time['time_slot'] for free_time in free_time_list if free_time['free']]
-    print('CALENDAR_ID:', CLENDAR_ID)
-    print('TIMEZONE:', TIMEZONE)
-    print('now:', now)
-    print('start_of_day:', start_of_day)
-    print('end_of_day:', end_of_day)
-    print('events:', events)
+
+    # 遍歷free_time_list並將時間顯示方式改為12小時制
+    for i in range(len(free_time_list)):
+        time_range = free_time_list[i]
+        start_time, end_time = time_range.split(" - ")
+        start_time_12hr = convert_time_12hr(start_time)
+        end_time_12hr = convert_time_12hr(end_time)
+        free_time_list[i] = f"{start_time_12hr} - {end_time_12hr}"
+
+
+##    return jsonify({'free_time': free_time_list,'start_of_day':[start_of_day],'end_of_day':[end_of_day],'events':[events],'busy_times':[busy_times],'now':[now],'cal_time':cal_time})
     return jsonify({'free_time': free_time_list})
 
+# 將24小時制的時間轉換成12小時制
+def convert_time_12hr(time_str):
+    time_obj = datetime.datetime.strptime(time_str, '%H:%M')
+    return time_obj.strftime("%I:%M %p")
+
+##    time_obj = datetime.datetime.strptime(time_str, "%H:%M")
+##    time_str_12h = datetime.datetime.strftime(time_obj, "%p %I:%M")
+##    time_str_cn = time_str_12h.replace("AM", u"早上").replace("PM", u"下午")
+##    return time_str_cn
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
